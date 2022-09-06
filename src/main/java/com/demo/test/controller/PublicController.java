cc02c586dc04f1bfca79d31a0d70ba231fa10f7c
付志高 <zhiti.fu@noitom.com>
2022-09-06 15:17:03 +0800

    更换提示语

--- src/main/java/com/demo/test/controller/PublicController.java
+++ src/main/java/com/demo/test/controller/PublicController.java
@@ -1,5 +1,6 @@
 package com.demo.test.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,13 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
  * @Description
  * @date 2022-09-06 14:13
  */
+@Slf4j
 @RestController("")
 @RequestMapping("/public/info")
 public class PublicController {
 
     @GetMapping("/getInfo")
     public String getInfo(){
-        System.out.println("进来了");
-        return "成功";
+        log.info("into");
+        return "welcome to test Jenkins";
     }
 }
